57f98aed3400e5b5208f21ff12deb640b62138aa
Knnth3 <kenkm963@gmail.com>
2020-10-01 23:07:59 -0700

    Added a base pickup prefab w/ some visual scripts

---
+++ UOP1_Project/Assets/Scripts/Visual/HoverObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverObject : MonoBehaviour
+{
+    // Public variables
+    public Transform Center = null;
+    [Min(0)]
+    public float Frequency = 1; // Units are in milli-units per Second
+    public Vector2 YBounds = Vector2.one; // Units are in milli-units
+
+    // Private variables
+    private Transform transformCache;
+
+    void Start()
+    {
+        transformCache = this.transform;
+    }
+
+    void Update()
+    {
+        // Don't calculate if there is no specified center
+        if (Center == null)
+        {
+            return;
+        }
+
+        float scalar = 2.0f * Mathf.PI * Frequency;
+        Vector3 deltaPosition = Vector3.up * Mathf.Sin(scalar * Time.time);
+
+        // Apply limits (optional: may be more optimal to just have an amplitude applied here)
+        // although this is a quick and easy way to evaluate simple collisions
+
+        // Using "Elvis Operator" rather than "IF" statement to reduce space
+        deltaPosition.y = (deltaPosition.y > 0) ? 
+            deltaPosition.y * YBounds.x : deltaPosition.y * YBounds.y;
+
+        transformCache.position = Center.position + deltaPosition;
+    }
+}
---
+++ UOP1_Project/Assets/Scripts/Visual/RotateObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    // Public variables
+    public float Speed = 30; // Units are in Degrees per Second
+    public Vector3 AxisOfRotation = Vector3.up;
+
+    // Private variables
+    private Transform transformCache;
+
+    void Start()
+    {
+        transformCache = this.transform;
+    }
+
+    void Update()
+    {
+        // Calculate and apply next rotation
+        Vector3 deltaRotation = (Speed * Time.deltaTime) * AxisOfRotation;
+        transformCache.rotation *= Quaternion.Euler(deltaRotation);
+    }
+}
